04428e869bd6e4ac75444004cc063ee7f3dc9e8e
LukasWedberg <131009406+LukasWedberg@users.noreply.github.com>
2024-02-04 21:54:15 -0500

    Turns out I'm very unfamiliar with moving UI elements around. Glad to be learning, though! Time to code in the win condition and then we're really set.

--- Intermediate GP task 2/Assets/Scripts/Beanbag.cs
+++ Intermediate GP task 2/Assets/Scripts/Beanbag.cs
@@ -5,10 +5,11 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using TMPro;
 
-public class Beanbag : MonoBehaviour
+public class Beanbag : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IDragHandler, IEndDragHandler, IBeginDragHandler
 {
     public Psuedo3DManager psuedo3dManager;
 
+    float requiredThrowStrength = 0.01f;
 
     [SerializeField]
     private Vector3 _3dPos;
@@ -28,7 +29,8 @@ public class Beanbag : MonoBehaviour
         
             _3dPos = value; 
             
-            float depthAlpha = 1-Mathf.Lerp(1f, psuedo3dManager.backgroundScale, Mathf.Clamp(_3dPos.z,-20,0)/psuedo3dManager.backgroundDepth);
+            float depthAlphaPos = 1-Mathf.Lerp(1f, psuedo3dManager.backgroundScale, Mathf.Clamp(_3dPos.z,psuedo3dManager.backgroundDepth, 0)/psuedo3dManager.backgroundDepth);
+            float depthAlphaScale = Mathf.Lerp(1f, psuedo3dManager.sizeScale, Mathf.Clamp(_3dPos.z,psuedo3dManager.backgroundDepth, 0)/psuedo3dManager.backgroundDepth);
 
             //Debug.Log("DEPTH ALPHA:"+ depthAlpha );
 
@@ -36,14 +38,14 @@ public class Beanbag : MonoBehaviour
             //the size and position to make it look
             //like it's moving in 3d!
 
-            _2dPositioning.anchoredPosition = new Vector2(
-                Mathf.Lerp(_3dPos.x, psuedo3dManager.depthDot.anchoredPosition.x, depthAlpha)
-                , Mathf.Lerp(_3dPos.y,psuedo3dManager.depthDot.anchoredPosition.y, depthAlpha)
+            _2dPositioning.localPosition = new Vector2(
+                Mathf.Lerp(_3dPos.x, psuedo3dManager.depthDot.anchoredPosition.x, depthAlphaPos)
+                , Mathf.Lerp(_3dPos.y,psuedo3dManager.depthDot.anchoredPosition.y, depthAlphaPos)
                 );
 
-            
 
-            _2dPositioning.localScale = foregroundScale * Mathf.Lerp(1f, psuedo3dManager.backgroundScale/2, depthAlpha);
+
+            _2dPositioning.localScale = foregroundScale * depthAlphaScale; //* Mathf.Lerp(1f, psuedo3dManager.sizeScale, depthAlphaScale * psuedo3dManager.scalingRatio);
 
 
         }
@@ -54,10 +56,12 @@ public class Beanbag : MonoBehaviour
     public enum BeanState { 
         Idle,
         Dragging,
-        Freefall
+        Freefall,
+        Landed
 
     }
 
+    BeanState currentBeanState = BeanState.Idle;
 
 
 
@@ -69,8 +73,15 @@ public class Beanbag : MonoBehaviour
     //lets go, then that means they want to throw it!
     [SerializeField]
     private Vector3 currentDraggingVelocity;
+    private Vector3 previousDraggingPos;
+
+    private float verticalVelocity = 0f;
+    private float horizontalVelocity = 0f;
+    private float ZVelocity = 0f;
 
 
+
+    private Vector3 startPos;
 
 
 
@@ -84,11 +95,171 @@ public class Beanbag : MonoBehaviour
         foregroundScale = _2dPositioning.localScale;
 
         ThreeDeePosition = _2dPositioning.anchoredPosition;
+
+        startPos = _2dPositioning.anchoredPosition;
+
+        Invoke("AllowSelect", 1);
     }
 
     // Update is called once per frame
     void Update()
     {
         //ThreeDeePosition += new Vector3(0,0,-0.1f);
+
+        switch (currentBeanState){
+        
+            case BeanState.Idle:
+                ThreeDeePosition = Vector3.Lerp(_2dPositioning.anchoredPosition, startPos + new Vector3(0, 2f * Mathf.Sin( (.5f * Time.realtimeSinceStartup + startPos.x) ),0) , 0.1f);
+                //Debug.Log("IDLE");
+                
+                break;
+            case BeanState.Dragging:
+                //Debug.Log("CURRENTLY DRAGGING!");
+
+                break;
+
+            case BeanState.Freefall:
+
+                if (ThreeDeePosition.y > psuedo3dManager.groundLevel)
+                {
+                    Debug.Log("WHEEEEEE");
+                    //ThreeDeePosition += currentDraggingVelocity + new Vector3(0, 0, -currentDraggingVelocity.magnitude);
+                    ThreeDeePosition += new Vector3(horizontalVelocity, verticalVelocity, -.8f) * Time.deltaTime * currentDraggingVelocity.magnitude * 6;
+
+                    verticalVelocity -= psuedo3dManager.gravity * Time.deltaTime;
+
+
+                    //currentDraggingVelocity = new Vector3(currentDraggingVelocity.x * .9f, currentDraggingVelocity.y - 0.1f, 0);
+                }
+                else {
+                    currentBeanState = BeanState.Landed;
+                
+                }
+
+                break;
+            case BeanState.Landed:
+                if (psuedo3dManager.beanBagObjects.Contains(this.gameObject) )
+                {
+                    psuedo3dManager.beanBagObjects.Remove(this.gameObject);
+                }
+
+                break;
+
+
+        }
+
+
     }
+
+    //bean selection status
+    bool mouseRollOver = false;
+    bool allowSelect = false;
+    bool isDragging = false;
+
+
+    //Mouse Roll Over 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (psuedo3dManager.selectedBeanbag == null)
+        {
+            mouseRollOver = true;
+
+            //cardManager.RelocateAllCards(this);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouseRollOver = false;
+        //rectTrans.localScale = Vector3.one;
+
+        //cardManager.RelocateAllCards();
+    }
+
+    //Drag Detection
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (allowSelect && eventData.button == PointerEventData.InputButton.Left && currentBeanState == BeanState.Idle)
+        {
+            if (!isDragging)
+            {
+                isDragging = true;
+
+                currentBeanState = BeanState.Dragging;
+                //cardManager.currentSelectedCard = this;
+                psuedo3dManager.selectedBeanbag = this;
+            }
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (allowSelect && eventData.button == PointerEventData.InputButton.Left)
+        {
+            if (isDragging)
+            {
+                
+                //rectTrans.localScale = selectedScale;
+                Vector3 globalMousePos;
+                if (RectTransformUtility.ScreenPointToWorldPointInRectangle(_2dPositioning, eventData.position, eventData.pressEventCamera, out globalMousePos))
+                {
+                    _2dPositioning.position = globalMousePos;
+
+                    //previousDraggingPos = globalMousePos;
+
+                    //Debug.Log("CURRENTLY DRAGGING!");
+
+                    currentDraggingVelocity = transform.position - previousDraggingPos;
+
+                    previousDraggingPos = transform.position;
+
+                    Debug.Log(currentDraggingVelocity.magnitude);
+
+
+                }
+            }
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (allowSelect && eventData.button == PointerEventData.InputButton.Left)
+        {
+            if (isDragging)
+            {
+                //End Drag
+                isDragging = false;
+
+                currentBeanState = BeanState.Idle;
+
+                //allowSelect = false;
+                //Invoke("AllowSelect", 0.5f);
+                //cardManager.currentSelectedCard = null;
+
+                psuedo3dManager.selectedBeanbag = null;
+                
+                
+                if (currentDraggingVelocity.magnitude > requiredThrowStrength)
+                {
+                    ThreeDeePosition = new Vector3(transform.position.x, transform.position.y,0);
+
+                    verticalVelocity = currentDraggingVelocity.y;
+
+                    horizontalVelocity = currentDraggingVelocity.x;
+
+                    currentBeanState = BeanState.Freefall;
+
+                }
+
+
+            }
+        }
+    }
+
+    void AllowSelect()
+    {
+        allowSelect = true;
+    }
+
+
 }
--- Intermediate GP task 2/Assets/Scripts/Psuedo3DManager.cs
+++ Intermediate GP task 2/Assets/Scripts/Psuedo3DManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Psuedo3DManager : MonoBehaviour
 {
@@ -10,16 +12,49 @@ public class Psuedo3DManager : MonoBehaviour
 
     public float backgroundDepth = -20;
 
+    public float sizeScale = 0.15f;
+
+    public float scalingRatio = 2;
+
+    public float gravity = 1;
+
+    public GameObject beanBagPrefab;
+
+    public List<GameObject> beanBagObjects = new List<GameObject>();
+
+
+    public float groundLevel = -55;
+
+    public Beanbag selectedBeanbag = null;
+
+    public Canvas canvas;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject currentBean = Instantiate(beanBagPrefab, canvas.transform);
+
+            beanBagObjects.Add(currentBean);
+
+            currentBean.transform.localPosition = new Vector2(-85 + 85 * i, -187);
+
+        }
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (beanBagObjects.Count == 0) {
+            Debug.Log("You've lost! Reloading room...");
+            
+            
+
         
+        }
     }
 }
